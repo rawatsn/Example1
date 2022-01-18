66562300ed6552a885369f060bd0f1377dd09040
rawatsn <rawatsn@mail.uc.edu>
2022-01-18 17:20:17 -0500

    updates

--- test1.R
+++ test1.R
@@ -5,16 +5,13 @@ names(data) <- header
 
 dim(data) 
 
-data$sample_id 
-
-
-data[data$sample_id=="BCL2",]
-
-data[,c(6)] 
 
 header2 <- scan(file, skip = 1, nlines = 1, sep="\t",      what = character())
 
+data$sample_id
+
 LumA <- data[,header2=='Luminal A']
+dim(LumA)
 
 Basal <- data[,header2=='Basal-like']
 dim(data) 
